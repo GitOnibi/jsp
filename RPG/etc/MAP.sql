3e207e0f3c869e337619ca8febda04b66e37bb33
onigawara <hicuri@naver.com>
2019-02-22 20:30:37 +0900

    commit

---
+++ RPG/etc/MAP.sql
@@ -0,0 +1,16 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-2월-22-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table MAP
+--------------------------------------------------------
+
+  CREATE TABLE "ONIGAWARA"."MAP" 
+   (	"IDX" NUMBER(3,0), 
+	"NAME" VARCHAR2(100 BYTE), 
+	"SUB" LONG
+   ) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+REM INSERTING into ONIGAWARA.MAP
+SET DEFINE OFF;
